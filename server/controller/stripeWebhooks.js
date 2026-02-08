f9c233bc35c5e7a1e1b028db64f1074d277e94d4
Mehul Gupta <mehulgupta2409@gmail.com>
2026-02-08 23:07:29 +0530

    updated stripeWebhook

--- server/controller/stripeWebhooks.js
+++ server/controller/stripeWebhooks.js
@@ -17,23 +17,37 @@ export const stripeWebhooks = async (req, res) => {
     }
     try {
         switch (event.type) {
-            case "payment_intent.succeeded":{
-                const paymentIntent = event.data.object;
-                const sessionList = await stripeInstance.checkout.sessions.list({
-                    payment_intent:paymentIntent.id
-                })
-                const session = sessionList.data[0]
-                const {bookingId} = session.metadata;
-                await Booking.findByIdAndUpdate(bookingId,{
-                    isPaid : true,
-                    paymentLink : ""
-                })
+            case "checkout.session.completed":{
+                // const paymentIntent = event.data.object;
+                // const sessionList = await stripeInstance.checkout.sessions.list({
+                //     payment_intent:paymentIntent.id
+                // })
+                // const session = sessionList.data[0]
+                // const {bookingId} = session.metadata;
+                // await Booking.findByIdAndUpdate(bookingId,{
+                //     isPaid : true,
+                //     paymentLink : ""
+                // })
 
-                //Send confirmation Email
+                // //Send confirmation Email
+                // await inngest.send({
+                //     name : 'app/show.booked',
+                //     data : {bookingId}
+                // })
+                const session = event.data.object;
+                const { bookingId } = session.metadata;
+
+                if (!bookingId) break;
+
+                await Booking.findByIdAndUpdate(bookingId, {
+                    isPaid: true,
+                    paymentLink: ""
+                });
+
                 await inngest.send({
-                    name : 'app/show.booked',
-                    data : {bookingId}
-                })
+                    name: "app/show.booked",
+                    data: { bookingId }
+                });
 
                 break;
             }
